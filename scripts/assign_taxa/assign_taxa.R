6f9d9038c72b29976042f824a1a92eac0215c979
William Wallén <haddock@rackham3.uppmax.uu.se>
2025-11-17 15:11:03 +0100

    changes for trimming reads

--- scripts/assign_taxa/assign_taxa.R
+++ scripts/assign_taxa/assign_taxa.R
@@ -1,8 +1,11 @@
 library("dada2")
 load("savepoint2.RData")
 
-taxa <- assignTaxonomy(seqtab.nochim, "./silva/silva_trainset.fa.gz", multithreading=TRUE)
-taxa <- addSpecies(taxa, "./silva/silva_v138.2_assignSpecies.fa.gz")
+silva_train <- "/proj/rnaseq01/viroids/Viroid-transcriptome-mining/slutkurs/silva/silva_trainset.fa.gz"
+silva_assign <- "/proj/rnaseq01/viroids/Viroid-transcriptome-mining/slutkurs/silva/silva_trainset.fa.gz"
+
+taxa <- assignTaxonomy(seqtab.nochim, silva_train, multithreading=TRUE)
+taxa <- addSpecies(taxa, silva_assign)
 taxa.print <- taxa # Removing sequence rownames for display only
 rownames(taxa.print) <- NULL
 head(taxa.print)
--- scripts/trimming/batch_dada2.sh
+++ scripts/trimming/batch_dada2.sh
@@ -2,7 +2,7 @@
 #SBATCH -A uppmax2025-2-344       # Replace with your project, e.g. sens2024012
 #SBATCH -p core                  # Use the 'core' partition for normal CPU jobs
 #SBATCH -n 10                     # Number of cores
-#SBATCH -t 5:00:00              # Wall time (2 hours here)
+#SBATCH -t 24:00:00              # Wall time (2 hours here)
 #SBATCH -J analysis_gm_w        # Job name
 #SBATCH -o logs/taxa_%j.out      # STDOUT (%j = job ID)
 #SBATCH -e logs/taxa_%j.err      # STDERR
@@ -13,7 +13,7 @@ module load R/4.3.1
 module load R_packages/4.3.1
 
 # -- Create a run directory where all checkpoints are saved
-RUN_DIR="runs/run_${SLURM_JOB_ID}"
+RUN_DIR="/home/haddock/private/Wolverine/runs/run_trimming_taxa_17112025_${SLURM_JOB_ID}"
 mkdir -p "$RUN_DIR"
 cp test_taxa.R savepoint1.RData "$RUN_DIR"/
 cd "$RUN_DIR"
@@ -21,8 +21,8 @@ cd "$RUN_DIR"
 mkdir -p logs
 
 # -- Run the scripts: 
-Rscript /home/haddock/private/Wolverine/filtering.R 
+Rscript /home/haddock/private/Wolverine/scripts/trimming/filtering.R 
 
-Rscript /home/haddock/private/Wolverine/error_and_chimeras.R 
+Rscript /home/haddock/private/Wolverine/scripts/trimming/error_and_chimeras.R 
 
-#Rscript assign_taxa.R 
+Rscript /home/haddock/private/Wolverine/scripts/assign_taxa/assign_taxa.R 

010749269c2cf00c850c6e2cab7a1024c87eefc6
hlovisa4 <lovisahamback@gmail.com>
2025-11-19 09:48:06 +0100

    edited dada2_to_ps.R

--- scripts/dada2_to_ps.R
+++ scripts/dada2_to_ps.R
@@ -3,11 +3,13 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install("dada2")
 library(dada2); packageVersion("dada2")
 
+BiocManager::install("phyloseq")
 library(phyloseq); packageVersion("phyloseq")
+BiocManager::install("Biostrings")
 library(Biostrings); packageVersion("Biostrings")
+BiocManager::install("ggplot2")
 library(ggplot2); packageVersion("ggplot2")
 theme_set(theme_bw())
-load("checkpoint3.RData")
 
 
 # 1. Read your metadata file
@@ -15,7 +17,10 @@ metadata <- read.csv("C:/Users/Lovisa/Documents/Skola/Tillampad_Bioinformatik/me
 
 # 2. Ensure column name matches the rownames of seqtab.nochim
 #    If it's called "NGI.ID" or similar, adjust here:
-colnames(metadata)[colnames(metadata) == "NGI ID"] <- "NGI_ID"
+rownames(seqtab.nochim) <- sub("^([^_]+_[^_]+).*", "\\1", rownames(seqtab.nochim))
+
+colnames(metadata)[colnames(metadata) == "NGI.ID"] <- "NGI_ID"
+
 
 # 3. Set NGI_ID as rownames
 rownames(metadata) <- metadata$NGI_ID
@@ -39,4 +44,4 @@ names(dna) <- taxa_names(ps)
 ps <- merge_phyloseq(ps, dna)
 taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
 ps
-
+save(ps, file="ps.RData")

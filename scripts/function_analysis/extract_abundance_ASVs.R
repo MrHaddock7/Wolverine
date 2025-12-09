ded3ca9e97daaada19ff9d50315d572ea60d8fed
MrHaddock7 <113512371+MrHaddock7@users.noreply.github.com>
2025-12-09 09:59:20 +0100

    new picrust with correct data

--- scripts/function_analysis/extract_abundance_ASVs.R
+++ scripts/function_analysis/extract_abundance_ASVs.R
@@ -4,18 +4,18 @@ library(Biostrings)
 source("config.R")
 
 
-path <- paste(home, "data/ps_no_ctrl_clean.RData", sep="")
+path <- paste(home, "data/ps_1126.RData", sep="")
 path
 
 load(path)
 
-seqs <- refseq(ps_no_ctrl)   # DNAStringSet med namn ASV1, ASV2, ...
+seqs <- refseq(ps_1126)   # DNAStringSet med namn ASV1, ASV2, ...
 
 # säkerställ att names(seqs) matchar taxa_names (ska de redan göra)
 names(seqs)[1:5]
-taxa_names(ps_no_ctrl)[1:5]
+taxa_names(ps_1126)[1:5]
 
-save_path <- paste(home, "data/ASV_abundance_sequences/rep_seqs.fna", sep="")
+save_path <- paste(home, "data/ASV_abundance_sequences/rep_seqs_1126.fna", sep="")
 
 # skriv fasta-fil till disk
 writeXStringSet(seqs, filepath = save_path)
@@ -25,18 +25,18 @@ writeXStringSet(seqs, filepath = save_path)
 library(biomformat)
 
 # Ta ut otutabellen som matrix
-otu_mat <- as(otu_table(ps_no_ctrl), "matrix")
+otu_mat <- as(otu_table(ps_1126), "matrix")
 
 # PICRUSt2 vill ha taxa som rader: kontrollera
-taxa_are_rows(ps_no_ctrl)
+taxa_are_rows(ps_1126)
 # Om FALSE, transponera:
-if (!taxa_are_rows(ps_no_ctrl)) {
+if (!taxa_are_rows(ps_1126)) {
   otu_mat <- t(otu_mat)
 }
 
 # Skapa BIOM-objekt
 otu_biom <- make_biom(data = otu_mat)
 
-save_path_abundance <- paste(home, "data/ASV_abundance_sequences/table.biom", sep="")
+save_path_abundance <- paste(home, "data/ASV_abundance_sequences/table_1126.biom", sep="")
 # Skriv till fil
 write_biom(otu_biom, save_path_abundance)

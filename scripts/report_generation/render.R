36e2b9ea90193e1bdeff2eddb1ccbe515a136253
hlovisa4 <lovisahamback@gmail.com>
2025-12-18 10:24:37 +0100

    upd reports

--- scripts/report_generation/render.R
+++ scripts/report_generation/render.R
@@ -21,11 +21,29 @@ zoos <- tibble(zoo = c(
 )) %>%
   pull(zoo)
 
+zoos_pdf <- tibble(zoo = c(
+  "Skansen",
+  "Helsinki",
+  "Gaia",
+  "Herberstein",
+  "Osnabrück",
+  "Duisburg",
+  "Salzburg",
+  "Nordens ark",
+  "Lycksele",
+  "Vildriket",
+  "Kolmarden",
+  "Boras",
+  "Ahtari"
+)) %>%
+  pull(zoo)
+
+
 zoos
 
 reports <- tibble(
   input = "C:/Users/Lovisa/Documents/Wolverine/scripts/report_generation/testing_pdf.Rmd",
-  output_file = str_glue("{zoos}.pdf"),
+  output_file = str_glue("{zoos_pdf}.pdf"),
   params = map(zoos, ~ list(zoo = .))
 )
 
